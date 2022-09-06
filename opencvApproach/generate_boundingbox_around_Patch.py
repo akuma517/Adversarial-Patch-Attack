100e36f054a58dabb4bb2a6b31b14601ee852274
akuma517 <ayush.nith17@gmail.com>
2022-09-06 19:10:02 +0200

    {Changes in opencv Approach}

--- opencvApproach/generate_boundingbox_around_Patch.py
+++ opencvApproach/generate_boundingbox_around_Patch.py
@@ -1,10 +1,11 @@
-from ast import main
+
 import cv2
 import numpy as np
 from skimage.morphology import rectangle
 import skimage.filters as filters
 import glob as glob
 import matplotlib.pyplot as plt
+from google.colab.patches import cv2_imshow
 
 #Method to read the images from a folder
 def readImagesFromFolder(root_dir_path):
@@ -17,18 +18,16 @@ def maximumSum(list1):
     return(sum(max(list1, key = sum)))
 
 
-
-
-def patch_detection(image_path):
+def patch_detection(image):
 
   # set the bounding box size
-  bbox_size = 4
+  bbox_size = 5
   alpha = 30 #Contrast control
   beta = -127 #Brightness control
-  expected_patch_height_lowerlimit = 6
-  expected_patch_height_upperlimit = 21
-  expected_patch_width_lowerlimit = 6
-  expected_patch_width_upperlimit = 21
+  expected_patch_height_lowerlimit = 8
+  expected_patch_height_upperlimit = 30
+  expected_patch_width_lowerlimit = 8
+  expected_patch_width_upperlimit = 30
  
   adjusted = cv2.convertScaleAbs(image, alpha=alpha, beta=beta)
 
@@ -39,7 +38,7 @@ def patch_detection(image_path):
   # to single channel 8 or 16-bits, not float
   # and variance will be larger than 8-bit range
   img = cv2.cvtColor(adjusted,cv2.COLOR_RGB2GRAY).astype(np.uint16)
-
+  
   # compute square of image
   img_sq = cv2.multiply(img, img)
 
@@ -51,41 +50,77 @@ def patch_detection(image_path):
 
   # compute square of local mean of img
   sq_mean_img = cv2.multiply(mean_img, mean_img)
-
+  
   # compute variance using float versions of images
   var = cv2.add(mean_img_sq.astype(np.float32), -sq_mean_img.astype(np.float32))
-
+  
   # compute standard deviation
   std = cv2.sqrt(var).clip(0,255).astype(np.uint16)
-
-  binary_mask = std  < 1
   
+  binary_mask = std  < 1
   result = binary_mask[...,None] * image
   #change the color of masked part from black to gray
   new_res = result.copy()
+  out_img = image.copy()
   #Find the bbox around the portion where the ADVERSARIAL PATCH was.
+  #Threshold value based on the adjusted grayscale image
+  (T, threshInv) = cv2.threshold(img, 80, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
+  print("[INFO] otsu's thresholding value: {}".format(T))
+
+
   img_gray = cv2.cvtColor(new_res, cv2.COLOR_BGR2GRAY)
-  ret, im = cv2.threshold(img_gray, 0, 1, cv2.THRESH_BINARY_INV)
+  ret, im = cv2.threshold(img_gray, 0, T, cv2.THRESH_BINARY_INV)
   contours, _  = cv2.findContours(im, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
   
+  x=0
+  y=0
+  w=0 
+  h = 0
   for c in contours:
   # compute the bounding box of the contour and then draw the
   # bounding box on both input images to represent where the two images differ
     (x, y, w, h) = cv2.boundingRect(c)
-    print(f'Height: {h} , Width: {w}')
-    if h>expected_patch_height_lowerlimit and h<expected_patch_height_upperlimit and w>expected_patch_width_lowerlimit and w<expected_patch_width_upperlimit:
-      cv2.rectangle(new_res, (x, y), (x + w, y + h), (128, 128, 128), -1)
+    area_of_rectangle = w*h
+    print(f'Height: {h} , Width: {w} , Area : {area_of_rectangle}')
 
-  return new_res,  (x, y, w, h)
+    """ Range of area of rectangle is estimated according to the size of the image """
+    if area_of_rectangle in list(range(200,360)):
+          if w>h:
+            w=h
+            cv2.rectangle(out_img, (x, y), (x + w, y + h), (128, 128, 128), -1)
+            print(f'From IF LOOP : Height: {h} , Width: {w}')
+          else:
+            h=w
+            cv2.rectangle(out_img, (x, y), (x + w, y + h), (128, 128, 128), -1)
+            print(f'From ELSE LOOP : Height: {h} , Width: {w}')
+    
+    
+
+
+    # if h in list(range(expected_patch_height_lowerlimit,expected_patch_height_upperlimit)):
+    #     if w in list(range(expected_patch_width_lowerlimit, expected_patch_width_upperlimit)):
+    #       if w>h:
+    #         w=h
+    #         cv2.rectangle(out_img, (x, y), (x + w, y + h), (128, 128, 128), -1)
+    #         print(f'From IF LOOP : Height: {h} , Width: {w}')
+    #       else:
+    #         h=w
+    #         cv2.rectangle(out_img, (x, y), (x + w, y + h), (128, 128, 128), -1)
+    #         print(f'From ELSE LOOP : Height: {h} , Width: {w}')
+      #cv2.rectangle(out_img, (x, y), (x + 14, y + 14), (128, 128, 128), -1)
+      
+
+  return out_img,  (x, y, w, h)
 
 
 if __name__ == '__main__':
-    root_dir_path = '/content/drive/MyDrive/Adversarial Patch/Mini Dataset/Patched_Train/0'
-    masked_path_dir_path = '/content/drive/MyDrive/Adversarial Patch/Mini Dataset/MaskedPatchOutput/0'
+    root_dir_path = '/content/drive/MyDrive/Adversarial Patch/Mini Dataset/patched_test'
+   # masked_path_dir_path = '/content/drive/MyDrive/Adversarial Patch/Mini Dataset/MaskedPatchOutput/1'
     image_path_list = readImagesFromFolder(root_dir_path)
     for image_path in image_path_list:
         image = cv2.imread(image_path) # Read image
-        new_res,_ = patch_detection(image_path)
+        print(image.shape)
+        new_res,_ = patch_detection(image)
         plt.figure()
         plt.subplot(1,2,1)
         plt.imshow( image)
@@ -93,7 +128,8 @@ if __name__ == '__main__':
         plt.subplot(1,2,2)
         plt.imshow(new_res)
         plt.show()
-        break
+        
+        
         #out_file_name = image_path.split('/')[-1][:-4]
         # cv2.imwrite(f'{masked_path_dir_path}/{out_file_name}.png',new_res)
   
